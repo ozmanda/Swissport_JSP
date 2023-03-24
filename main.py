66371766054691da05e131e2079e6d2f1cb7a87a
ozmanda <usher.juliapatricia@gmail.com>
2023-03-24 11:17:39 +0100

    JSSEnv test

--- main.py
+++ main.py
@@ -4,5 +4,18 @@ import argparse
 
 
 if __name__ == '__main__':
-    env = gym.make('JSSEnv:jss-v1')
+    env = gym.make('jss-v1', env_config={'instance_path': 'venv/Lib/site-packages/JSSEnv/envs/instances/test1'})
+    observation = env.reset()
+
+    for t in range(100):
+        env.render()
+        print(observation)
+        action = env.action_space.sample()
+        observation, reward, done, info = env.step(action)
+        print(f'Observation:\n{observation}')
+        print(f'Reward: {reward}')
+        if done:
+            print(f'finished after {t} timesteps')
+
+
     x=5
