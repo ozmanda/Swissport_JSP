af88113691300ab2b1135872e770e50c9fb670f4
ozmanda <usher.juliapatricia@gmail.com>
2023-04-18 16:41:18 +0200

    Adjusted to load DJSPEnv

--- main.py
+++ main.py
@@ -1,21 +1,25 @@
 import gym
-import JSSEnv
+import os
+from DJSP_env import DJSPEnv
 import argparse
 
 
 if __name__ == '__main__':
-    env = gym.make('jss-v1', env_config={'instance_path': 'venv/Lib/site-packages/JSSEnv/envs/instances/test1'})
+    parser = argparse.ArgumentParser()
+    parser.add_argument('instancepath', type=str, help='Relative path to instance specification')
+    args = parser.parse_args()
+    assert os.path.isfile(args.instancepath)
+
+    env = DJSPEnv(instance_path=args.instancepath)
     observation = env.reset()
 
     for t in range(100):
         env.render()
         print(observation)
         action = env.action_space.sample()
-        observation, reward, done, info = env.step(action)
+        observation, reward, done = env.step(action)
         print(f'Observation:\n{observation}')
         print(f'Reward: {reward}')
         if done:
             print(f'finished after {t} timesteps')
 
-
-    x=5
