0f68a3367527fc01f2b928934dbfb79915f2843a
ozmanda <usher.juliapatricia@gmail.com>
2023-04-25 15:08:13 +0200

    Per-machine schedule visualisation, coloured by aircraft

--- vis_utils.py
+++ vis_utils.py
@@ -1,2 +1,31 @@
 import matplotlib
+import numpy as np
+import plotly.express as px
 
+def machine_assignment_dict(assignment, machines_per_op, aircraft_info, operation_times):
+    """
+    Converts a scheduling assignment to a dictionary suited for usage with ff.create_gantt()
+    """
+    assignment_dict = []
+    machine_index = 0
+    for op_type in range(len(machines_per_op)):
+        for nmachine in range(machines_per_op[op_type]):
+            machine_name = f'Machine {op_type}.{nmachine}'
+
+            # gather vector of aircraft assignments for this machine and operation type
+            assignments = np.where(assignment[:, op_type, machine_index])[0]
+            for aircraft_idx in assignments:
+                assignment_dict.append({'Machine': machine_name,
+                                        'Start': operation_times[aircraft_idx, op_type]['Scheduled Start'],
+                                        'Finish': operation_times[aircraft_idx, op_type]['Scheduled End'],
+                                        'Aircraft': f'{aircraft_info[aircraft_idx]["REG"]}',
+                                        'Delay': 0})
+
+
+            machine_index += 1
+    return assignment_dict
+
+def render_schedule(assignment, machines_per_op, aircraft_info, operation_times):
+    assignment_dict = machine_assignment_dict(assignment, machines_per_op, aircraft_info, operation_times)
+    fig = px.timeline(assignment_dict, x_start='Start', x_end='Finish', y='Machine', color='Aircraft')
+    fig.show()
