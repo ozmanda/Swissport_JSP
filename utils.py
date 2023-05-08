0857553dbf5d11c86f3ab1b8a8bb5a9c8f70a5a6
ozmanda <usher.juliapatricia@gmail.com>
2023-05-08 09:01:10 +0200

    minutes since midnight, maximum ac delay

--- utils.py
+++ utils.py
@@ -3,3 +3,13 @@ from pandas import to_datetime, Timedelta
 def minutes_since_midnight(time):
     midnight = to_datetime('00:00', format='%H:%M')
     return int((time - midnight) / Timedelta(minutes=1))
+
+
+def minutes_to_midnight(time):
+    midnight = to_datetime('00:00', format='%H:%M')
+    return int((midnight - time) / Timedelta(minutes=1))
+
+
+def calculate_maximum_delay(aircraft):
+    total_max_delay = 0
+    for ac in aircraft:
