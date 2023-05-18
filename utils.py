323975fa797b7ce91a17f4f1ff55e1fa7a6b6cb2
ozmanda <usher.juliapatricia@gmail.com>
2023-05-18 10:24:52 +0200

    minutes to midngiht and max delay calculation

--- utils.py
+++ utils.py
@@ -6,10 +6,12 @@ def minutes_since_midnight(time):
 
 
 def minutes_to_midnight(time):
-    midnight = to_datetime('00:00', format='%H:%M')
+    midnight = to_datetime('00:00', format='%H:%M') + Timedelta(days=1)
     return int((midnight - time) / Timedelta(minutes=1))
 
 
 def calculate_maximum_delay(aircraft):
     total_max_delay = 0
     for ac in aircraft:
+        total_max_delay += minutes_to_midnight(aircraft[ac]['STD'])
+    return total_max_delay
